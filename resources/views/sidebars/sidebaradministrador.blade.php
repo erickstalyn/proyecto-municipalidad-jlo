c6c40ffa658d1e97fec15f8dcb203450d54a3613
Stalyn <espacherrezpuyen@gmail.com>
2020-01-27 19:40:13 -0500

    Avance

---
+++ resources/views/sidebars/sidebaradministrador.blade.php
@@ -0,0 +1,73 @@
+<ul class="sidebar-menu" id="nav-accordion">
+    <p class="centered"><a href="#"><img src="img/logo_mjlo.jpg" class="img-circle" width="80"></a></p>
+    <h5 class="centered">Sistema Interno</h5>
+    <li class="mt">
+      <a class="active" href="#">
+        <i class="fas fa-gamepad"></i>
+        <span>Inicio</span>
+        </a>
+    </li>
+    <li class="sub-menu">
+      <a href="javascript:;">
+        <i class="fa fa-desktop"></i>
+        <span>UI Elements</span>
+        </a>
+      <ul class="sub">
+        <li><a href="#">General</a></li>
+        <li><a href="#">Buttons</a></li>
+        <li><a href="#">Panels</a></li>
+        <li><a href="#">Font Awesome</a></li>
+      </ul>
+    </li>
+    <li class="sub-menu">
+      <a href="javascript:;">
+        <i class="fa fa-cogs"></i>
+        <span>Components</span>
+        </a>
+      <ul class="sub">
+        <li><a href="#">Grids</a></li>
+        <li><a href="#">Calendar</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Todo List</a></li>
+        <li><a href="#">Dropzone File Upload</a></li>
+        <li><a href="#">Inline Editor</a></li>
+        <li><a href="#">Multiple File Upload</a></li>
+      </ul>
+    </li>
+    <li class="sub-menu">
+      <a href="javascript:;">
+        <i class="fa fa-book"></i>
+        <span>Extra Pages</span>
+        </a>
+      <ul class="sub">
+        <li><a href="#">Blank Page</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Lock Screen</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Invoice</a></li>
+        <li><a href="#">Pricing Table</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">404 Error</a></li>
+        <li><a href="#">500 Error</a></li>
+      </ul>
+    </li>
+    <li class="sub-menu">
+      <a href="javascript:;">
+        <i class="fa fa-tasks"></i>
+        <span>Forms</span>
+        </a>
+      <ul class="sub">
+        <li><a href="#">Form Components</a></li>
+        <li><a href="#">Advanced Components</a></li>
+        <li><a href="#">Form Validation</a></li>
+        <li><a href="#">Contact Form</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">
+        <i class="fa fa-envelope"></i>
+        <span>Mail </span>
+        <span class="label label-theme pull-right mail-info">2</span>
+        </a>
+    </li>
+  </ul>
