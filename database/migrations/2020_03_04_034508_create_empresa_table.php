f3463ecdb75676c1754f5a10828e9eaffe988826
Stalyn <espacherrezpuyen@gmail.com>
2020-03-04 01:08:17 -0500

    Persona y empresa ready

---
+++ database/migrations/2020_03_04_034508_create_empresa_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->smallIncrements('id'); // usa 2 bytes - máximo 32767 datos
+            $table->char('ruc', 11)->unique();
+            $table->string('razon_social', 350)->unique();
+            $table->string('nombre_comercial', 350);
+            $table->string('tipo', 200)->nullable();
+            $table->string('condicion', 45);
+            $table->string('estado', 45);
+            $table->integer('area', 10)->nullable();
+            $table->string('sector', 300)->nullable();
+            $table->date('fecha_inscripcion')->nullable();
+            $table->date('inicio_actividades')->nullable();
+            $table->json('act_comerciales')->nullable();
+            $table->json('repre_legales')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
---
+++ database/migrations/2020_03_04_034713_create_persona_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persona', function (Blueprint $table) {
+            $table->smallIncrements('id'); // usa 2 bytes - máximo 32767 datos
+            $table->char('dni', 8);
+            $table->string('nombres', 120);
+            $table->string('apellidos', 120);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persona');
+    }
+}
